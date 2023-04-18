2adf01bb346bddfb846c1c94ed8b1f536061f7c7
pmezquita <ing.pedromezquita@gmail.com>
2023-04-18 14:53:57 -0600

    [ADD] Pantalla por-capital.component

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Country} from "../../interfaces/pais.interface";
+import {PaisService} from "../../services/pais.service";
 
 @Component({
   selector: 'app-por-capital',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
   ]
 })
 export class PorCapitalComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
+  constructor(private paisService: PaisService) {
+  }
+
+  buscar(termino: string) {
+    this.termino = termino;
+    this.hayError = false;
+
+    this.paisService.buscarCapital(termino)
+      .subscribe({
+        next: paises => {
+          this.paises = paises
+        }, error: () => {
+          this.hayError = true;
+          this.paises = [];
+        }
+      });
+  }
 }
