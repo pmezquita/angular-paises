3b509f1f39f5e18327f5e627b599458a48b616fa
pmezquita <ing.pedromezquita@gmail.com>
2023-04-19 15:23:39 -0600

    [ADD] Se implementan sugerencias en el input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,12 +5,21 @@ import {Country} from "../../interfaces/pais.interface";
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: []
+  styles: [`
+    li {
+      cursor: pointer;
+    }
+  `]
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+
+  get mostrarSugerencia() {
+    return this.paisesSugeridos.length != 0;
+  }
 
   constructor(private paisService: PaisService) {
   }
@@ -18,6 +27,7 @@ export class PorPaisComponent {
   buscar(termino: string) {
     this.termino = termino;
     this.hayError = false;
+    this.paisesSugeridos = [];
 
     this.paisService.buscarPais(termino)
       .subscribe({
@@ -32,6 +42,12 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false;
-    console.log(termino);
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino)
+      .subscribe({
+        next: paises => this.paisesSugeridos = paises.splice(0, 5),
+        error: () => this.paisesSugeridos = []
+      });
   }
 }
