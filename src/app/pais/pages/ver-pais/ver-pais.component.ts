f2c6132dd09c8a1f13d0647f8682dc5f99b040dc
pmezquita <ing.pedromezquita@gmail.com>
2023-04-19 09:18:51 -0600

    [REF] Se implementa switchMap

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {PaisService} from "../../services/pais.service";
+import {switchMap} from "rxjs";
 
 @Component({
   selector: 'app-ver-pais',
@@ -16,15 +17,25 @@ export class VerPaisComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
     this.activatedRoute.params
-      .subscribe(({id}) => {
-        console.log(id);
-
-        this.paisSrvice.getPaisPorCode(id).subscribe(paises => {
-          console.log(paises[0]);
-        });
-
+      .pipe(
+        switchMap(({id}) => this.paisSrvice.getPaisPorCode(id))
+      )
+      .subscribe(paises => {
+        console.log(paises[0]);
       });
+
+    // this.activatedRoute.params
+    //   .subscribe(({id}) => {
+    //     console.log(id);
+    //
+    //     this.paisSrvice.getPaisPorCode(id)
+    //       .subscribe(paises => {
+    //         console.log(paises[0]);
+    //       });
+    //
+    //   });
   }
 
 }
