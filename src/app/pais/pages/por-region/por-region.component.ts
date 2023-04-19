430295a61ab50a8c0edb018517ba28a5969399cd
pmezquita <ing.pedromezquita@gmail.com>
2023-04-19 11:00:32 -0600

    [ADD] ngClass

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`
+    button {
+      margin-right: 5px;
+      margin-bottom: 5px;
+    }
+  `]
 })
 export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+
+  constructor() {
+  }
+
+  activarRegion = (region: string) => {
+    this.regionActiva = region;
+    // TODO: hacer el llamado al servicio
+  }
 
 }
